6468132de6f929ce61d40434248b7194771c8763
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-15 17:00:41 -0600

    -- Buliding out View for notes, not handling error correctly yet

--- NotesXWA/Views/NoteListView.swift
+++ NotesXWA/Views/NoteListView.swift
@@ -9,8 +9,38 @@ import SwiftUI
 import SwiftData
 
 struct NoteListView: View {
+    
+    @Environment(\.modelContext) private var context
+    @Query(sort: \.createdAt, order: .reverse) var allNotes: [Note]
+    @State var noteText = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section {
+                DisclosureGroup("Create a note") {
+                    TextField("Enter text", text: $noteText, axis: .vertical)
+                        .lineLimit(2...4)
+                    
+                    Button("Save") {
+                        createNote()
+                    }
+                }
+            }
+            
+            Section {
+                if allNotes.isEmpty {
+                    ContentUnavailableView("You do not have any notes yet.", systemImage: "note")
+                } else {
+                    
+                }
+            }
+        }
+    }
+    
+    func createNote() {
+        let note = Note(id: UUID().uuidString, content: noteText, createdAt: .now)
+        context.insert(note)
+         try? context.save()
     }
 }
 
