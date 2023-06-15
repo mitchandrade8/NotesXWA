1b3e41de63cc62790adba5e9a7d393a7c54e0a77
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-15 16:25:23 -0600

    Initial Commit

---
+++ NotesXWA/NotesXWAApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesXWAApp.swift
+//  NotesXWA
+//
+//  Created by Mitch Andrade on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct NotesXWAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
