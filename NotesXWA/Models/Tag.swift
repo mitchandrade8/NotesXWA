2958084a0731591b341ca1df06640bed949428f3
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-15 16:47:31 -0600

    -- Finished - Models and Setting up containers for app -- Starting - Views

---
+++ NotesXWA/Models/Tag.swift
@@ -0,0 +1,8 @@
+//
+//  Tag.swift
+//  NotesXWA
+//
+//  Created by Mitch Andrade on 6/15/23.
+//
+
+import Foundation
---
+++ NotesXWA/Views/NoteListView.swift
@@ -0,0 +1,18 @@
+//
+//  NoteListView.swift
+//  NotesXWA
+//
+//  Created by Mitch Andrade on 6/15/23.
+//
+
+import SwiftUI
+
+struct NoteListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoteListView()
+}
